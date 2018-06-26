db8a0bb0f17294d7e35d334cd873a57ce1dd1fa5
Daniel Liburd <daniel.liburd@gmail.com>
2018-06-26 08:04:10 +0100

    Add catch to promise

--- src/feelo.js
+++ src/feelo.js
@@ -4,10 +4,13 @@ const configTwitter = require('./config').twitter;
 const chalk = require('chalk');
 
 bot()
-  .get('search/tweets', { q: `"${configTwitter.keyphrase}"`, count: 5 })
+  .get('search/tweetss', { q: `"${configTwitter.keyphrase}"`, count: 5 })
   .then(result =>
     result.data.statuses.map(s => {
       console.log('screen_name:', chalk.yellow(s.user.screen_name));
       console.log('tweet:', chalk.cyan(s.text));
     })
-  );
+  )
+  .catch(err => {
+    throw new Error(err);
+  });
